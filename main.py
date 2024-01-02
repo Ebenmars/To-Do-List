649ea8c8ed33be441ea3f165ce547e7fd91553e4
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2024-01-01 19:50:37 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+prompt = "Enter A Todo: "
+# getting user input
+task = input(prompt)
+task2 = input(prompt)
+task3 = input(prompt)
+# this is called a list
+todos = [task, task2, task3]
+
+print(todos)
+# this will output the type an argument
+print(type(task))
