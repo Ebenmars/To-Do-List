2e34484cfc0db146797e11879b22d8ed2bcb43c7
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2024-01-03 19:36:29 -0500

    Promptting User

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-prompt = "Enter A Todo: "
-# getting user input
-task = input(prompt)
-task2 = input(prompt)
-task3 = input(prompt)
-# this is called a list
-todos = [task, task2, task3]
 
-print(todos)
-# this will output the type an argument
-print(type(task))
+# declarations has to be done in one line
+prompt = "Type add, show, or exit: "
+# Creates empty List
+tasks = []
+
+while True:
+
+    # this indentation is important
+
+    # getting user input
+    user_action = input(prompt)
+    # this removes trailing zeros
+    user_action = user_action.strip()
+
+# is the user types add that means they want to add
+#  else they want to show the list of tasks
+    match user_action:
+        case "add":
+            task = input("Enter a todo: ")
+            # Adds on to list
+            tasks.append(task)
+        case "show":
+            for item in tasks:
+                print(item)
+        case "exit":
+            break
+print("Bye")
+
+#  this is called a list
+# todos = [task, task2, task3]
+#  this will output the type an argument
+# print(type(task))
